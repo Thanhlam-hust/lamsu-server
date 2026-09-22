1c9b0dd02b2a480cc3c7db74ef6bd141bdc7f5f7
Thanhlam-hust <buithanhlam1812@gmail.com>
2026-09-22 17:45:01 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        message: "Hello API!"
+    }));
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://192.168.4.108:${PORT}`);
+});
